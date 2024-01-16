7d020e7bc6a0460a98954988109310d605b2f5aa
focusjordan <156718308+focusjordan@users.noreply.github.com>
2024-01-16 01:44:23 -0500

    text box

---
+++ audioj/src/components/TextComponentOne.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="text-container">
+      <div class="text-box">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </div>
+      <div class="text-box">
+        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TextComponent',
+    // You can define props, data, methods, etc. here in the future
+  };
+  </script>
+  
+  <style scoped>
+  .text-container {
+    display: flex;
+    justify-content: space-around;
+  }
+  
+  .text-box {
+    border: 1px solid #ccc;
+    padding: 10px;
+    margin: 5px;
+  }
+  </style>
+  
