008d678639c3d377ef803fecb9dc1ca6f0e1c153
focusjordan <156718308+focusjordan@users.noreply.github.com>
2024-01-15 22:31:17 -0500

    text

---
+++ audioj/src/components/CenterText.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex justify-center items-center h-screen">
+      <span class="text-center text-xl font-semibold">
+        <slot></slot> <!-- Slot for custom text -->
+      </span>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'CenteredText'
+  }
+  </script>
