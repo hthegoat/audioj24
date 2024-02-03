f9eabfc220446a46511a12644c3f7668e8762041
focusjordan <156718308+focusjordan@users.noreply.github.com>
2024-02-03 17:02:12 -0500

    launch

--- audioj/src/components/TextComponentOne.vue
+++ audioj/src/components/TextComponentOne.vue
@@ -1,5 +1,5 @@
 <template>
-   <div id="learn" class="mx-auto max-w-screen-xl px-4 py-2 lg:flex lg:h-screen lg:items-center">
+   <div id="learn" class="mx-auto max-w-screen-xl px-4 py-2 lg:flex lg:h-screen lg:items-center"></div>
     <div class="mx-auto max-w-xl text-center">
             <div class="w-full mx-auto">
                 <h2>Upload your music. Sell it to your fans.</h2>
