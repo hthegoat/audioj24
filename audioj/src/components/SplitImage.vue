689e06bab3924fbadf5e8153fac7c20f9497e376
focusjordan <156718308+focusjordan@users.noreply.github.com>
2024-02-03 17:54:54 -0500

    yup

--- audioj/src/components/SplitImage.vue
+++ audioj/src/components/SplitImage.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
+    <div></div>
+
       <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
         <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
           <div class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
@@ -27,8 +28,9 @@
           </div>
         </div>
       </div>
+
     </div>
-    
+
   </template>
 <script>
 
